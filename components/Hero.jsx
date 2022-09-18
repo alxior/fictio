03e63f64d9d197d5f2fdd65669853470acda6491
alxior <willmakk123@gmail.com>
2022-09-18 13:14:00 +0200

    Made partners in hero section responsive.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -11,8 +11,8 @@ const Hero = () => {
     <div className="w-full overflow-hidden font-poppins mt-16 select-none cursor-default">
       <div className="container mx-auto px-4">
         {/* GRID */}
-        <div className="grid space-y-16 xl:space-y-0 lg:grid-cols-5 lg:grid-rows-2 xl:grid-rows-5">
-          <div className="text-center self-center xl:self-auto lg:text-left lg:col-span-2 lg:pt-20 xl:row-span-3">
+        <div className="grid space-y-16 xl:space-y-0 lg:grid-cols-5 lg:grid-rows-6 xl:grid-rows-5">
+          <div className="text-center self-center xl:self-auto lg:text-left lg:col-span-2 lg:pt-20 lg:row-span-4 xl:row-span-3">
             <p className="text-[28px] xs:text-[36px] sm:text-[42px] md:text-[48px] lg:text-[36px] xl:text-[48px] text-darkBlue font-semibold">
               Servicios De Nueva
             </p>
@@ -29,54 +29,35 @@ const Hero = () => {
           <Image
             src={heroPicture}
             alt="hero"
-            className="px-4 lg:col-span-3 xl:row-span-5 xl:self-center lg:px-0 xl:pl-16 xl:pb-16"
+            className="px-4 lg:col-span-3 lg:row-span-4 xl:row-span-5 xl:self-center lg:px-0 xl:pl-16 xl:pb-16"
           />
-          <p className="lg:col-span-5 xl:col-span-2 lg:px-32 text-center font-poppins text-[20px] md:text-[24px] xl:p-0 xl:pt-4 xl:text-left xl:text-[20px] xl:row-span-2 text-darkGray">
+          <p className="lg:col-span-5 lg:row-span-2 xl:col-span-2 lg:px-32 text-center font-poppins text-[20px] md:text-[24px] xl:p-0 xl:pt-4 xl:text-left xl:text-[20px] xl:row-span-2 text-darkGray lg:pt-6">
             Las metodologías más avanzadas para alcanzar los mejores resultados.
             En realidad no sabemos muy bien a qué nos dedicamos, esta empresa no
             existe.
           </p>
         </div>
-        {/* <div className="flex flex-row py-24 mt-4">
-          <div className="w-2/5">
-            <p className="font-poppins text-[48px] text-darkBlue font-semibold">
-              Servicios De Nueva
-            </p>
-            <p className="font-poppins text-[56px] text-fictioRed font-extrabold">
-              GENERACIÓN
-            </p>
-            <p className="font-poppins text-[48px] text-darkBlue font-semibold">
-              Para El Profesional
-            </p>
-            <p className="font-poppins text-[56px] text-fictioRed font-extrabold">
-              MODERNO
-            </p>
-            <br />
-            <p className="font-poppins text-[20px] text-darkGray">
-              Las metodologías más avanzadas para alcanzar los mejores
-              resultados. En realidad no sabemos muy bien a qué nos dedicamos,
-              esta empresa no existe.
-            </p>
-          </div>
-          <Image
-            src={heroPicture}
-            alt="Fictio Hero"
-            className="w-3/5 h-auto pl-16"
-          />
-        </div> */}
       </div>
       {/* Associated Brands */}
-      {/* <div className="w-full flex justify-center">
-        <div className="flex flex-row w-full max-w-[900px] justify-between">
+      <div className="container mx-auto px-4 mt-20 xl:px-32 xl:mt-12">
+        <div className="flex flex-row justify-between xs:px-8">
           <Image
             src={mastercard}
             alt="mastercard"
-            className="h-[65px] w-auto"
+            className="h-[40px] xs:h-[50px] md:h-[60px] xl:h-[65px] w-auto"
           />
-          <Image src={paypal} alt="paypal" className="h-[65px] w-auto" />
-          <Image src={stripe} alt="stripe" className="h-[65px] w-auto" />
+          <Image
+            src={paypal}
+            alt="paypal"
+            className="h-[40px] xs:h-[50px] md:h-[60px] xl:h-[65px] w-auto"
+          />
+          <Image
+            src={stripe}
+            alt="stripe"
+            className="h-[40px] xs:h-[50px] md:h-[60px] xl:h-[65px] w-auto"
+          />
         </div>
-      </div> */}
+      </div>
       {/* Stats */}
       {/* <div className="w-full flex justify-center bg-lightOrange h-[120px] mt-12">
         <div className="flex flex-row w-full max-w-[1280px] justify-between">
